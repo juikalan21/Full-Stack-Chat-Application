18ee33304f44d89b0c3127c10e40210cce2a4c09
juikalan <juikalan21@gmail.com>
2025-06-23 08:34:27 +0530

    solving errors

--- src/main/java/com/chat/app/model/ChatMessage.java
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -1,14 +1,39 @@
 package com.chat.app.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChatMessage {
     private String id;
     private String sender;
     private String content;
+
+    public ChatMessage() {}
+
+    public ChatMessage(String id, String sender, String content) {
+        this.id = id;
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
